0713cd8a483a170b825d0133f811f896ec9ebf43
codingsh.eth <codingsh@pm.me>
2021-02-26 18:43:26 -0300

    feat(multisig): add contracts

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Ditto MultiSig
+
+[base gnosis/safe-contracts](https://github.com/gnosis/safe-contracts)
---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "solidity-coverage";
+import "hardhat-deploy";
+import dotenv from "dotenv";
+import type { HttpNetworkUserConfig } from "hardhat/types";
+import yargs from "yargs";
+
+dotenv.config();
+const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK } = process.env;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+};
+
